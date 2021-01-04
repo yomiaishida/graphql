0b6aa9034325c130a8677d4351a9a40619c427a3
Abayomi Aishida <yomicorperate@gmail.com>
2021-01-04 16:22:16 +0100

    connecting to mongodb with mongoose

--- server/app.js
+++ server/app.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://abayomi123:abayomi123@cluster0.pkt3y.mongodb.net/abayomi123?retryWrites=true&w=majority",
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 app.use(
   "/graphql",
