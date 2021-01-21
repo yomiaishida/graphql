5b8a06651b22197d455c4b2d00df7bd4af22664d
Abayomi Aishida <yomicorperate@gmail.com>
2021-01-21 03:20:31 +0100

    Rendering queried data on dom

--- gqlclient/src/components/BookList.js
+++ gqlclient/src/components/BookList.js
@@ -12,13 +12,22 @@ const getBooksQuery = gql`
 `;
 
 class BookList extends Component {
+  displayBooks() {
+    let data = this.props.data;
+
+    if (data.loading) {
+      return <div>Loading books</div>;
+    } else {
+      return data.books.map((book) => {
+        return <li key={book.id}>{book.name}</li>;
+      });
+    }
+  }
+
   render() {
-    console.log(this.props);
     return (
       <div>
-        <ul id="book-list">
-          <li>Book name</li>
-        </ul>
+        <ul id="book-list">{this.displayBooks()}</ul>
       </div>
     );
   }
