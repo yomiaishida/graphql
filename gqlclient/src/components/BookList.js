3cad0d9d15de1b3e6e5c0a97354b11d2594036c4
Abayomi Aishida <yomicorperate@gmail.com>
2021-01-23 02:31:42 +0100

    Displaying bookdetails on click

--- gqlclient/src/components/BookList.js
+++ gqlclient/src/components/BookList.js
@@ -4,6 +4,9 @@ import { getBooksQuery } from "../queries/queries";
 import BookDetails from "./BookDetails";
 
 class BookList extends Component {
+  state = {
+    selected: null,
+  };
   displayBooks() {
     let data = this.props.data;
 
@@ -11,7 +14,16 @@ class BookList extends Component {
       return <div>Loading books</div>;
     } else {
       return data.books.map((book) => {
-        return <li key={book.id}>{book.name}</li>;
+        return (
+          <li
+            key={book.id}
+            onClick={(e) => {
+              this.setState({ selected: book.id });
+            }}
+          >
+            {book.name}
+          </li>
+        );
       });
     }
   }
@@ -20,7 +32,7 @@ class BookList extends Component {
     return (
       <div>
         <ul id="book-list">{this.displayBooks()}</ul>
-        <BookDetails />
+        <BookDetails bookId={this.state.selected} />
       </div>
     );
   }
