61fc80f880b80e5ec81f5749ed278dfbf09f85f9
Abayomi Aishida <yomicorperate@gmail.com>
2021-01-23 01:51:06 +0100

    Bookdetails compunent and getbookquery

--- gqlclient/src/components/BookList.js
+++ gqlclient/src/components/BookList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import { getBooksQuery } from "../queries/queries";
+import BookDetails from "./BookDetails";
 
 class BookList extends Component {
   displayBooks() {
@@ -19,6 +20,7 @@ class BookList extends Component {
     return (
       <div>
         <ul id="book-list">{this.displayBooks()}</ul>
+        <BookDetails />
       </div>
     );
   }
